3189e6a24e9b8cff42b7c93ec3c87bdf5962c279
ZHANGHA3 <HARPER.H.P.ZHANG@OOCL.COM>
2018-08-24 16:53:57 +0800

    refactor test

--- src/main/test/park/SmartBoyTest.java
+++ src/main/test/park/SmartBoyTest.java
@@ -22,15 +22,26 @@ public class SmartBoyTest {
 
 
     @Test
-    public void should_return_parkName_when_input_car(){
+    public void should_return_parkName_when_input_many_car(){
         for(int i=0;i<20;i++) {
             //given
             Car car = new Car("粤C GE43"+i);
             //when
             String parkName = boy.parkCar(car);
             //then
-            System.out.println(parkName);
+            //System.out.println(parkName);
             Assert.assertNotNull(parkName);
         }
+    }
+
+    @Test
+    public void should_return_car_when_input_carNum(){
+        boy.parkCar(new Car("粤C ABC123"));
+        //given
+        String carNum="粤C ABC123";
+        //when
+        Car car= boy.pickCar(carNum);
+        //then
+        Assert.assertNotNull(car);
     }
 }
